4b46de25442466aaf744d16e191ea7a5d2d1f8c9
Jon <jstapleton463@harrisonburg.k12.va.us>
2020-03-11 16:11:50 -0400

    srt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home">
-
-    
-    <img src="http://placekitten.com/200/100">
+    <img src="http://placekitten.com/400/800" />
+    <img src="http://placekitten.com/200/100" />
     <h1>Welcome to HHS</h1>
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-    <input>
+    <p>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum."
+    </p>
+    <input />
 
+    <div>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
   </div>
 </template>
