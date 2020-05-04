bbcc6f6a8c1aeff21cb063c9e8e32cd8c678369f
staplejm <staplejm@dukes.jmu.edu>
2020-05-04 09:57:43 -0400

    Simplified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,8 @@
 <template>
   <div class="home">
-    <img src="http://placekitten.com/400/800" />
-    <img src="http://placekitten.com/200/100" />
-    <h1>Welcome to HHS</h1>
-    <p>
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum."
-    </p>
-    <input />
+    
+    <h1>This is our app!</h1>
+    <p>Here are some words to demonstrate the paragraph tag</p>
 
-    <div>
-      <b-button>Button</b-button>
-      <b-button variant="danger">Button</b-button>
-      <b-button variant="success">Button</b-button>
-      <b-button variant="outline-primary">Button</b-button>
-    </div>
   </div>
 </template>
